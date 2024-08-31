4e40250ac45de4951f290d324485c5a3a73d7482
Menna Ashraf <menna.ashraf@holouly.co>
2024-08-31 21:48:28 +0300

    ff

--- api/models.py
+++ api/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-
+#mmm
     def __str__(self):
         return f"{self.title} by {self.author}"
